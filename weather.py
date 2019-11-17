e715a4a6a640e01f64d00601f8089a05d4a96091
Cristian <crystian_marica@yahoo.com>
2019-11-17 16:07:54 +0200

    weather_v2

--- weather.py
+++ weather.py
@@ -1,4 +1,16 @@
+import requests
+
+from secrets import app_id
+
+# def weather():
+#     print(">> Execut logica super complicata")
+#     w = {"Cluj": 15}
+#     return w
+
 def weather():
-    print(">> Execut logica super complicata")
-    w = {"Cluj": 15}
-    return w
+    url = f"http://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca&appid={app_id}&units=metric"
+    response = requests.get(url)
+    weather_cluj = response.json()
+    print(">>", weather_cluj)
+    temp_cluj = weather_cluj["main"]["temp"]
+    return {"Cluj-Napoca": round(temp_cluj)}
